f718de8cdef76b909c25fc65cf885b680b136cda
Masa Jobara <wolf.masa@gmail.com>
2017-01-15 01:05:14 +0900

    add first watch app project

---
+++ TimeWatchApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TimeWatchApp WatchKit Extension
+//
+//  Created by JobaraMasashi on 2017/01/14.
+//  Copyright © 2017年 ProjectJ. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ TimeWatchAppTests/TimeWatchAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  TimeWatchAppTests.swift
+//  TimeWatchAppTests
+//
+//  Created by JobaraMasashi on 2017/01/14.
+//  Copyright © 2017年 ProjectJ. All rights reserved.
+//
+
+import XCTest
+@testable import TimeWatchApp
+
+class TimeWatchAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
